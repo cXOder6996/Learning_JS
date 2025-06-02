e1321a5f53e93c0613fa387badbcd9a3f6ed4e39
cXOder6996 <guptakrish6996i@gmail.com>
2025-06-02 19:11:18 +0530

    DAY 2 : JS DataTypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,16 @@
+"use strict"; //treats all code as new version
+
+// alert(3 + 3) // we are using nodeJS not browser
+
+console.log(3 + 3)
+
+console.log("Krish")
+
+let name = "krish"
+let age = 18
+let isLogged = false
+//null => standalone value
+
+console.log(typeof undefined);
+console.log(typeof null);
+
