5b5a9c4444039aed9871d1febf652c9735d9e1ef
cXOder6996 <guptakrish6996i@gmail.com>
2025-08-24 15:36:55 +0530

    conv & DataType summary

---
+++ 01_basics/04_Conversions.js
@@ -0,0 +1,20 @@
+console.log(2 > 3);
+console.log("2" === 2)
+
+//Some more on datatypes
+//Primitive
+// 7 types : String, Number, Boolean, Null, undefined, Symbol, BigInt
+
+//JS is dynamically typed language(the type is determined at runtime)
+
+// Reference type(NON - Primitive)
+// Array, Objects, Functions
+
+let myObj = function(){
+    console.log("Hey there!");
+}
+
+console.log(typeof myObj); //gives function
+console.log(typeof null); //gives object
+
+
