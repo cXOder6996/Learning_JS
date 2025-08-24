dd785d22c60d44ade74edd20b9756088e49d164d
cXOder6996 <guptakrish6996i@gmail.com>
2025-08-24 14:40:56 +0530

    some changes

--- 01_basics/03_DataTypeConversion.js
+++ 01_basics/03_DataTypeConversion.js
@@ -1,11 +1,11 @@
 let score = true
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valInNum = Number(score)
-console.log(typeof valInNum);
-console.log(valInNum);
+// console.log(typeof valInNum);
+// console.log(valInNum);
 
 //"33" => 33
 // "33abc"=> NaN
@@ -13,7 +13,7 @@ console.log(valInNum);
 
 let flag = 0
 let boolF = Boolean(flag)
-console.log(boolF);
+// console.log(boolF);
 
 // 1 or any number except 0 => T
 // 0 gives F
@@ -21,4 +21,19 @@ console.log(boolF);
 
 let someNum = 69
 let someStr = String(someNum)
-console.log(typeof someStr);
+// console.log(typeof someStr);
+
+// ********Operations*********
+
+let value = 69
+let neg = -value
+// console.log(neg);
+
+let s1 = "Krish"
+let s2 = " Gupta"
+let s3 = s1 + s2
+// console.log(s3);
+
+console.log(true + true); // gives 1 + 1 = 2
+
+
