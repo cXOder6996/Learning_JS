e740b2758b1c4ed3d18690908292a9dcd2bed615
cXOder6996 <guptakrish6996i@gmail.com>
2025-09-17 19:35:46 +0530

    math & num

---
+++ 01_basics/06_MathNUM.js
@@ -0,0 +1,21 @@
+const score = new Number(400)
+// console.log(score.toString().length);
+// console.log(score.toFixed(2));
+
+const n1 = 123.69777
+// console.log(n1.toPrecision(3));
+
+const n2 = 100000
+// console.log(n2.toLocaleString());
+// console.log(Math.abs(-69));
+// console.log(Math.round(6.90));
+// console.log(Math.floor(6.90));
+// console.log(Math.ceil(6.39));
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const mini = 10
+const maxi = 20
+
+console.log(Math.floor(Math.random() * (maxi - mini + 1)) + mini);
+
